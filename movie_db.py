8c8ffe3fad8e8a84792dea0256123990c5a9a83c
Fatemeh Chamani <fch199674@yahoo.com>
2025-08-09 11:14:36 +0330

    update queries

--- movie_db.py
+++ movie_db.py
@@ -9,11 +9,18 @@ cursor.execute("""
         MovieID INTEGER PRIMARY KEY AUTOINCREMENT, 
         Title TEXT NOT NULL,
         ReleaseDate DATE,
-        DirectorID INTEGER
+        Studio VARCHAR (255)
     )
     """)
 
-
+cursor.execute("""
+    INSERT INTO Movies (MovieID, Title, ReleaseDate, Studio) VALUES
+    (1, 'Titanic', 1997, '20th Century Fox, Paramount Pictures'),
+    (2, '3 Idiots', 2009, 'Vinod Chopra Films'),
+    (3, 'The Dark Knight', 2008, 'Warner Bros. Pictures, Legendary Pictures, Syncopy'),
+    (4, 'Batman Begins', 2005, 'Warner Bros. Pictures, Legendary Pictures, Syncopy, DC Comics, Patalex III Productions');
+        
+""")
 
 conn.commit()
 conn.close()
